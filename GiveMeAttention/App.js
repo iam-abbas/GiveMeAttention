037e398ff0b27c9e3c3eb96d7ea9ea70d9978a1c
iam-abbas <iamabbas7@gmail.com>
2020-05-20 14:27:22 -0700

    disabled warnings

--- GiveMeAttention/App.js
+++ GiveMeAttention/App.js
@@ -14,6 +14,7 @@ import auth from '@react-native-firebase/auth';
 signOutUser = () => {
   auth().signOut();
 };
+console.disableYellowBox = true;
 
 const AppTabNavigator = createStackNavigator({
   Home: {
